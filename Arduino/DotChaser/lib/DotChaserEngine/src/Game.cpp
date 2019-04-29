2bfd25c6adf07a58ef8a47dde5492a9098e0e87c
Jan Ryklikas <jry@facelift-bbt.com>
2019-04-30 01:54:02 +0200

    1st iteration of collision detection

--- Arduino/DotChaser/lib/DotChaserEngine/src/Game.cpp
+++ Arduino/DotChaser/lib/DotChaserEngine/src/Game.cpp
@@ -11,25 +11,64 @@
  * Game implementation
  */
 
-
-/**
- * @param time
- * @param size
- * @param players
- */
-Game::Game(double time, int size, Player *players[4]) {
-
+Game::Game(Player** players, Item** items) {
+  _items = items;
+  _players = players;
 }
 
-/**
- * item collision and player collision
- *
- * @param player
- */
-void Game::checkCollision(Player *player) {
+void Game::playerButtonPressed(int playerID) {
+  if (checkPlayerCollision(playerID)) {
+    Serial.println("Player Collision detected!!");
+  } else if (checkItemCollision(playerID)) {
+    Serial.println("Item Collision detected!!");
+  } else {
+    _players[playerID]->changeDirection();
+  }
+}
 
+bool Game::checkPlayerCollision(int playerID) {
+  //check player Collision
+  Player *currentPlayer = _players[playerID];
+  //iterate players
+  for ( int i = 0 ; i < MAX_PLAYERS ; i++ ) {
+    Player *enemyPlayer = _players[i];
+    //check if enemyPlayer is not currentPlayer and is not empty
+    if ( i != playerID && enemyPlayer != nullptr ) {
+      //check player positions
+      if ( currentPlayer->position == enemyPlayer->position ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool Game::checkItemCollision(int playerID) {
+  //check item collision
+  Player *currentPlayer = _players[playerID];
+  //iterate players
+  for ( int i = 0 ; i < MAX_ITEMS ; i++ ) {
+    Item *item = _items[i];
+    //check if item is not empty
+    if ( item != 0 ) {
+      //check player/item positions
+      if ( currentPlayer->position == item->position ) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 void Game::update() {
+
+}
+
+
+void Game::_playerCollide(int player1ID, int player2ID) {
+
+}
+
+void Game::_itemCollide(int playerID, int itemID) {
 
 }
--- Arduino/DotChaser/lib/DotChaserEngine/src/Game.h
+++ Arduino/DotChaser/lib/DotChaserEngine/src/Game.h
@@ -8,47 +8,49 @@
 #ifndef _GAME_H
 #define _GAME_H
 
-#include <Arduino.h>
-#include <FastLED.h>
-#include <Game.h>
-#include <Player.h>
-#include <Item.h>
+  #define MAX_PLAYERS 4
+  #define MAX_ITEMS 64
 
-class Game {
-public:
+  #include <Arduino.h>
+  #include <FastLED.h>
+  #include <Game.h>
+  #include <Player.h>
+  #include <Item.h>
 
-/**
- * @param time
- * @param size
- * @param players
- */
-Game(double time, int size, Player *players[4]);
-
-/**
- * item collision and player collision
- *
- * @param player
- */
-void checkCollision(Player *player);
-
-void update();
-private:
-   /**
-   * remaining time
-   * initial time counts down
-   */
-  double time;
-  int fieldSize;
-  Player *players[4];
-  Item *items[255];
   /**
-  * array with all player positions? to check collisions
-  */
-  int playerField[255];
-  /**
-  * array with all item positions? to check collisions
-  */
-  int itemField[255];
-};
+  Checkcollision
+  Array mit spielern
+  Array mit items
+
+  Check player position
+  Compare with all player positions
+
+  Check player position
+  Check with all item positions
+
+  Game object gets time as input from engine
+  Game changes speed step in engine
+  Game checks collision
+  Game generates items
+  Game only changes item/player attributes
+  Engine just renders item
+  And player objects
+
+  **/
+
+  class Game {
+  public:
+    Game(Player** players, Item** items);
+    void playerButtonPressed(int playerID);
+    bool checkPlayerCollision(int playerID);
+    bool checkItemCollision(int playerID);
+    void update();
+  private:
+    Player** _players;
+    Item** _items;
+    void _playerCollide(int player1ID, int player2ID);
+    void _itemCollide(int playerID, int itemID);
+    //int time
+  };
 
 #endif //_GAME_H
